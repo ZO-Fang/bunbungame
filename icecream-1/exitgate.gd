154bd0428744f30d145948e5b204485a8d46091e
Zhou Fang <zoefang@protonmail.com>
2025-11-02 00:34:20 +0000

    fix bug: use call_deferred for scene switching to prevent physics flush errors

--- icecream-1/exitgate.gd
+++ icecream-1/exitgate.gd
@@ -11,6 +11,33 @@
 		#print("切换到 bg2")
 	## 如果想要更多层，可以继续处理 bg2 显示 bg3 等
 
+#extends Area2D
+#
+#@export var next_scene_path: String = "res://bg2.tscn"
+#@export var player_new_position: Vector2
+#
+#func _on_body_entered(body):
+	#if body.name == "player":
+		#switch_map()
+#
+#func switch_map():
+	#var map_container = get_node("/root/base/Node2D")
+	#
+	## 清空容器内的所有地图
+	#for child in map_container.get_children():
+		#child.queue_free()
+	#
+	## 加载并添加新地图
+	#var new_map = load(next_scene_path).instantiate()
+	#map_container.add_child(new_map)
+	#
+	## 重置玩家位置
+	#if player_new_position != Vector2.ZERO:
+		#var player = get_node("/root/base/player")
+		#player.global_position = player_new_position
+	#
+	#print("切换到: ", next_scene_path)
+	
 extends Area2D
 
 @export var next_scene_path: String = "res://bg2.tscn"
@@ -18,9 +45,10 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.name == "player":
-		switch_map()
+		# 使用 call_deferred 来安全地切换场景
+		call_deferred("deferred_switch_map")
 
-func switch_map():
+func deferred_switch_map():
 	var map_container = get_node("/root/base/Node2D")
 	
 	# 清空容器内的所有地图
@@ -36,4 +64,4 @@ func switch_map():
 		var player = get_node("/root/base/player")
 		player.global_position = player_new_position
 	
-	print("切换到: ", next_scene_path)
+	print("切换到: ", next_scene_path)	
